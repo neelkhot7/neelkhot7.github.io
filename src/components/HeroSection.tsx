e9519966dc853249f7236b102592af7591149e16
Neel Narendra Khot <neelkhot@outlook.com>
2025-07-25 22:40:52 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -117,9 +117,7 @@ const HeroSection = () => {
       </div>
 
       {/* Scroll Indicator */}
-      <br>
-      </br>
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-20">
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-20">
         <div className="w-6 h-10 border-2 border-secondary rounded-full flex justify-center">
           <div className="w-1 h-3 bg-secondary rounded-full mt-2 animate-bounce" />
         </div>
