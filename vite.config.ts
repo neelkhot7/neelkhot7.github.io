e8ca60c226c0cf0e8a55a9bbe42534da3045914b
neelkhot7 <neelkhot@outlook.com>
2025-07-27 10:07:37 +0530

    Add deploy config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/neelkhot7.github.io/", // 👈 This is critical for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
